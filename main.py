580e38f60f20707fccb5f92c14d954d431bd44d8
RuiVieira89 <rui.vieira.vv@gmail.com>
2020-12-24 11:10:04 +0000

    env setup

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+#!/home/rui/anaconda3/envs/tf-gpu/bin/python
+
+import numpy as np
+import torch
+
+def Encurvadura(x):
+
+	'''Encurvadura'''
+	l = x[0] # 0.4 # heigth [m]
+	e = x[1] # 0.001 # force eccentricity[m], 0 if none 
+	P = 10 # force [N]
+	E = 117e9 # elastcity module ·[Pa] (copper)
+	r = 1/100 # [m]
+	I = (np.pi/4)*r**4
+	k = np.sqrt(np.abs(P)/(E*I))
+
+	flexa = e*(1-np.cos(k*l))/np.cos(k*l) # flexion [m]
+	Mf_max = P*(e+flexa) # maximum moment of flextion
+	W = l*(np.pi*r**2) # volume [mn]
+	tension_max = P/(np.pi*r**2) + Mf_max/W
+
+	return flexa, tension_max
 
 
 
-
-
-def func(x):
+print(Encurvadura([1, 1]))
